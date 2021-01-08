6a947290761418a93a5d8c3d5c26d4b01b64b340
asbp <guungofficial@gmail.com>
2021-01-08 21:46:29 +0700

    first commit

---
+++ Assets/GerakanPlayer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GerakanPlayer : MonoBehaviour
+{
+    public float speed;
+    Rigidbody rb;
+    Animator animator;
+
+    public Transform playerTrans;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Gerak();
+    }
+
+    void Gerak()
+    {
+        float gerak = Input.GetAxis("Horizontal");
+
+        rb.velocity = Vector3.right * gerak * speed;
+        animator.SetFloat("Kecepatan", Mathf.Abs(gerak), .1f, Time.deltaTime);
+
+        playerTrans.localEulerAngles = new Vector3(0, gerak * 90, 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("virus"))
+        {
+            Time.timeScale = 0;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/VirusGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirusGenerator : MonoBehaviour
+{
+    public GameObject virus;
+    public float minTime, maxTime;
+    public float minPos, maxPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(VirusSpawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator VirusSpawn()
+    {
+        Instantiate(virus, transform.position + Vector3.right * Random.Range(minPos, maxPos), Quaternion.identity);
+        yield return new WaitForSeconds(Random.Range(minTime, maxTime));
+        StartCoroutine(VirusSpawn());
+    }
+}
